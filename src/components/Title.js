eddfc5dc92ef481163923eb30f1dfe372f34b1be
MrQuartzer <nantawat_yai@hotmail.com>
2022-05-22 14:23:33 +0700

    Add react-switch

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,19 @@
+import { useState } from "react"
+import Switch from "react-switch"
+
 const Title = () =>{
+    const [theme,setTheme] = useState(false)
+
+    const toggleSwitch = (checked) =>{
+        setTheme(checked)
+    }
     return(
         <header>
-            <span>Mode</span>
+            <span>Mode[{theme}]</span>
+            <Switch
+                onChange={toggleSwitch}
+                checked={theme}
+            />
         </header>
     )
 }
